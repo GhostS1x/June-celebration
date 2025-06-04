75879ebf616e1a1b6bbe74c93242a53662150894
gustavo.umbelino <gustavo.umbelino@gpnet.com.br>
2025-06-04 13:02:25 -0300

    diminuindo o tamanho

--- fj-front-web/src/app/pages/coi19sj29/page.jsx
+++ fj-front-web/src/app/pages/coi19sj29/page.jsx
@@ -179,7 +179,8 @@ export default function Comidas() {
             alimentos.map((item) => (
               <li
                 key={item}
-                className="bg-yellow-100  w-56 p-4 grid grid-cols-1 rounded-md shadow-md h-36 items-center"
+                className="bg-yellow-100  w-5
+                46 p-4 grid grid-cols-1 rounded-md shadow-md h-36 items-center"
               >
                 <span className="text-black">{item}</span>
                 <button
